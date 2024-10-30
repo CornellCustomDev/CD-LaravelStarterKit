06b9f989fcaa4132dacee2a6fac620dbc376ef86
Eric Woods <woodseowl@gmail.com>
2024-10-30 17:11:46 -0400

    Linting

--- src/CUAuth/routes.php
+++ src/CUAuth/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use CornellCustomDev\LaravelStarterKit\CUAuth\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/shibboleth-login', [AuthController::class, 'shibbolethLogin'])->name('cu-auth.shibboleth-login');
