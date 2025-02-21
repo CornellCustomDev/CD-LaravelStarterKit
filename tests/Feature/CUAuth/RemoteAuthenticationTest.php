60a66bea0e0db71afdcb4d31a640b8cbc25c64cb
Eric Woods <woodseowl@gmail.com>
2025-02-21 17:44:59 -0500

    Remove unused test

--- tests/Feature/CUAuth/RemoteAuthenticationTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace CornellCustomDev\LaravelStarterKit\Tests\Feature\CUAuth;
-
-use CornellCustomDev\LaravelStarterKit\Tests\Feature\FeatureTestCase;
-
-class RemoteAuthenticationTest extends FeatureTestCase {}
