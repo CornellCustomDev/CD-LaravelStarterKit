1288247efdde66b8f157cc062f1b7efc5f6958e5
Eric Woods <woodseowl@gmail.com>
2025-02-21 09:04:25 -0500

    Fix test for github action

--- tests/Feature/CUAuth/PhpSamlTest.php
+++ tests/Feature/CUAuth/PhpSamlTest.php
@@ -3,6 +3,7 @@
 namespace CornellCustomDev\LaravelStarterKit\Tests\Feature\CUAuth;
 
 use CornellCustomDev\LaravelStarterKit\CUAuth\Managers\SamlIdentityManager;
+use CornellCustomDev\LaravelStarterKit\StarterKitServiceProvider;
 use CornellCustomDev\LaravelStarterKit\Tests\Feature\FeatureTestCase;
 use OneLogin\Saml2\Error;
 use OneLogin\Saml2\Settings;
@@ -29,6 +30,12 @@ class PhpSamlTest extends FeatureTestCase
 
     public function testCanGetSsoUrl()
     {
+        $this->artisan(StarterKitServiceProvider::PACKAGE_NAME.':install')
+            ->expectsQuestion('What would you like to install or update?', ['certs'])
+            ->expectsConfirmation('Proceed with installation?', 'yes')
+            ->expectsOutputToContain('Installation complete.')
+            ->assertSuccessful();
+
         $url = SamlIdentityManager::getSsoUrl('/');
 
         $this->assertStringContainsString('https://shibidp-test.cit.cornell.edu/idp/profile/SAML2/Redirect/SSO?SAMLRequest=', $url);
